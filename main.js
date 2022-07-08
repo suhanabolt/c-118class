5dd3dabb673d87f97d601bbbac6d4767e7f3201a
suhanabolt <106386090+suhanabolt@users.noreply.github.com>
2022-07-08 19:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet')
+}
+
+function setup() 
+{
+    canvas=createCanvas(280,280)
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(classifyCanvas)
+    synth=window.speechSynthesis
+}
+
+function draw()
+ {
+    strokeWeight(7)
+    stroke("yellow")
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)  
+    }
+}
+function clearCanvas() 
+{
+    background("white")
+}
+function classifyCanvas() 
+{
+    classifier.classify(canvas,gotResult)
+}
+function gotResult(error,results) {
+    if (error) 
+    {
+        console.error(error)
+    }
+    console.log(results)
+    document.getElementById('label').innerHTML = 'label:' +results[0].label
+    document.getElementById('confidence').innerHTML = 'confidence:' +Math.round(results[0].confidence*100)+ '%'
+
+    utterThis= new SpeechSynthesisUtterance(results[0].label)
+    synth.speak(utterThis)
+
+
+}
